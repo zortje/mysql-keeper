9ed654828a11f7e52c1da9b20566734d3e2ca9d9
René Tønner Stokholm <zortje@gmail.com>
2015-01-11 01:13:40 +0100

    Adding column class with check for auto_increment without primary key

---
+++ src/Database/Table/Column.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Zortje\MySQLKeeper\Database\Table;
+
+/**
+ * Class Column
+ *
+ * @package Zortje\MySQLKeeper\Database\Table
+ */
+class Column {
+
+	/**
+	 * @var string
+	 */
+	private $field;
+
+	/**
+	 * @var string
+	 */
+	private $type;
+
+	/**
+	 * @var string
+	 */
+	private $null;
+
+	/**
+	 * @var string
+	 */
+	private $key;
+
+	/**
+	 * @var null|string
+	 */
+	private $default;
+
+	/**
+	 * @var string
+	 */
+	private $extra;
+
+	/**
+	 * @var array
+	 */
+	private $result;
+
+	/**
+	 * @param array $column Column information
+	 */
+	public function __construct($column) {
+		$this->field   = $column['Field'];
+		$this->type    = $column['Type'];
+		$this->null    = $column['Null'];
+		$this->key     = $column['Key'];
+		$this->default = $column['Default'];
+		$this->extra   = $column['Extra'];
+	}
+
+	public function getResult() {
+		/**
+		 * auto_increment checks
+		 */
+		if ($this->extra === 'auto_increment') {
+			if ($this->key !== 'PRI') {
+				$this->result[] = 'An auto_increment column should have a primary key';
+			}
+		}
+
+		return $this->result;
+	}
+
+}
---
+++ tests/Database/Table/ColumnTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Zortje\MySQLKeeper\Tests\Database\Table;
+
+use Zortje\MySQLKeeper\Database\Table\Column;
+
+/**
+ * Class ColumnTest
+ *
+ * @package Zortje\MySQLKeeper\Tests\Database\Table
+ */
+class ColumnTest extends \PHPUnit_Framework_TestCase {
+
+	public function testIncorrectAutoIncrementKey() {
+		$row = [
+			'Field'   => 'id',
+			'Type'    => 'int(10) unsigned',
+			'Null'    => 'NO',
+			'Key'     => 'MUL',
+			'Default' => '',
+			'Extra'   => 'auto_increment'
+		];
+
+		$column = new Column($row);
+
+		$result = $column->getResult();
+
+		$this->assertGreaterThan(0, count($result));
+		$this->assertTrue(in_array('An auto_increment column should have a primary key', $result));
+	}
+
+}
