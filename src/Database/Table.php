9b3cc0d876dedb49ff378bd85d6c06b4d2d9de83
René Tønner Stokholm <zortje@gmail.com>
2015-01-11 01:43:23 +0100

    Adding table class

---
+++ src/Database/Table.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Zortje\MySQLKeeper\Database;
+
+use Zortje\MySQLKeeper\Database\Table\Column;
+
+/**
+ * Class Table
+ *
+ * @package Zortje\MySQLKeeper\Database
+ */
+class Table {
+
+	/**
+	 * @var string
+	 */
+	private $table;
+
+	/**
+	 * @var \PDO
+	 */
+	private $pdo;
+
+	/**
+	 * @var array
+	 */
+	private $result = [];
+
+	/**
+	 * @param string $table Table name
+	 * @param \PDO   $pdo   Database connection
+	 */
+	public function __construct($table, \PDO $pdo) {
+		$this->table = $table;
+		$this->pdo   = $pdo;
+	}
+
+	/**
+	 * Get result of column
+	 *
+	 * @return array Result
+	 */
+	public function getResult() {
+		foreach ($this->pdo->query('SHOW COLUMNS FROM `users`;') as $row) {
+			$column = new Column($row);
+
+			foreach ($column->getResult() as $result) {
+				$this->result[] = $result;
+			}
+		}
+
+		return $this->result;
+	}
+}
