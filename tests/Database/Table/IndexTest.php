ae0e26daf9137d77625bc9d316050e07702f7446
René Tønner Stokholm <zortje@gmail.com>
2015-01-17 22:14:38 +0100

    Adding tests

--- tests/Database/Table/IndexTest.php
+++ tests/Database/Table/IndexTest.php
@@ -2,6 +2,8 @@
 
 namespace Zortje\MySQLKeeper\Tests\Database\Table;
 
+use Zortje\MySQLKeeper\Database\Table\Index;
+
 /**
  * Class IndexTest
  *
@@ -9,4 +11,39 @@ namespace Zortje\MySQLKeeper\Tests\Database\Table;
  */
 class IndexTest extends \PHPUnit_Framework_TestCase {
 
+	public function testGetKeyName() {
+		$index = new Index('id', null);
+
+		$this->assertSame('id', $index->getKeyName());
+	}
+
+	public function testGetColumns() {
+		$index = new Index(null, ['id']);
+
+		$this->assertSame(['id'], $index->getColumns());
+	}
+
+	public function testIsDuplicate() {
+		$indexAlpha = new Index(null, ['id']);
+		$indexBeta  = new Index(null, ['id']);
+
+		$this->assertTrue($indexAlpha->isDuplicate($indexBeta));
+		$this->assertTrue($indexBeta->isDuplicate($indexAlpha));
+	}
+
+	public function testIsDuplicateMultiple() {
+		$indexAlpha = new Index(null, ['id', 'active']);
+		$indexBeta  = new Index(null, ['id', 'active']);
+
+		$this->assertTrue($indexAlpha->isDuplicate($indexBeta));
+		$this->assertTrue($indexBeta->isDuplicate($indexAlpha));
+	}
+
+	public function testIsDuplicateDifferentOrdering() {
+		$indexAlpha = new Index(null, ['id', 'active']);
+		$indexBeta  = new Index(null, ['active', 'id']);
+
+		$this->assertFalse($indexAlpha->isDuplicate($indexBeta));
+		$this->assertFalse($indexBeta->isDuplicate($indexAlpha));
+	}
 }
