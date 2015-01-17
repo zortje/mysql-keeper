fec973a6bf2f4cf2a4f7fe1158f1379feb297d26
René Tønner Stokholm <zortje@gmail.com>
2015-01-17 15:43:50 +0100

    Adding getters and isDuplicate functions

--- src/Database/Table/Index.php
+++ src/Database/Table/Index.php
@@ -24,7 +24,38 @@ class Index {
 	 * @param string[] $columns Index column names
 	 */
 	public function __construct($keyName, $columns) {
-		$this->keyName  = $keyName;
-		$this->$columns = $columns;
+		$this->keyName = $keyName;
+		$this->columns = $columns;
+	}
+
+	/**
+	 * Get Index key name
+	 *
+	 * @return string Index key name
+	 */
+	public function getKeyName() {
+		return $this->keyName;
+	}
+
+	/**
+	 * Get Index column names
+	 *
+	 * @return string[] Index column names
+	 */
+	public function getColumns() {
+		return $this->columns;
+	}
+
+	/**
+	 * Check if given Index is duplicate of this
+	 *
+	 * @param Index $index Index
+	 *
+	 * @return bool TRUE if duplicate, otherwise FALSE
+	 */
+	public function isDuplicate(Index $index) {
+		$duplicate = $this->getColumns() === $index->getColumns();
+
+		return $duplicate;
 	}
 }
