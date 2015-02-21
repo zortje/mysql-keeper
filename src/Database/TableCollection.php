d5cd3dcaad973b39ea69989cd22322c03ced2121
René Tønner Stokholm <zortje@gmail.com>
2015-02-21 23:32:59 +0100

    Fixing error in class check

--- src/Database/TableCollection.php
+++ src/Database/TableCollection.php
@@ -24,7 +24,7 @@ class TableCollection extends Collection {
 	 * @throws \InvalidArgumentException if incorrect object type is added
 	 */
 	public function add($table) {
-		if (!is_object($table) || get_class($table) !== 'Zortje\MySQLKeeper\Database\Table\Table') {
+		if (!is_object($table) || get_class($table) !== 'Zortje\MySQLKeeper\Database\Table') {
 			$argumentType = is_object($table) ? get_class($table) : gettype($table);
 
 			throw new \InvalidArgumentException(sprintf('Collection may only contain "%s" objects, "%s" is not allowed', 'Zortje\MySQLKeeper\Database\Table', $argumentType));
