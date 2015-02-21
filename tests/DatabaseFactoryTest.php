fd950c58fc6fb65fec4d6fcf283ea3bab60efe9d
René Tønner Stokholm <zortje@gmail.com>
2015-02-21 23:46:40 +0100

    Adding test for getResult on database

--- tests/DatabaseFactoryTest.php
+++ tests/DatabaseFactoryTest.php
@@ -46,4 +46,43 @@ class DatabaseFactoryTest extends \PHPUnit_Framework_TestCase {
 			}
 		}
 	}
+
+	public function testDatabaseResults() {
+		$this->pdo->query(file_get_contents('tests/Database/files/nodes.sql'));
+		$this->pdo->query(file_get_contents('tests/Database/files/users.sql'));
+
+		$database = DatabaseFactory::create($this->pdo);
+
+		$result = $database->getResult();
+
+		$expected = [
+			[
+				'type'        => 'index',
+				'key'         => 'unique',
+				'description' => 'An unique index on the primary key column is redundant'
+			],
+			[
+				'type'        => 'index',
+				'key'         => 'key',
+				'description' => 'An key index on the primary key column is redundant'
+			],
+			[
+				'type'        => 'column',
+				'field'       => 'id',
+				'description' => 'Set as auto_increment but has no primary key'
+			],
+			[
+				'type'        => 'index',
+				'key'         => 'id_active2',
+				'description' => 'Is duplicate of id_active'
+			],
+			[
+				'type'        => 'column',
+				'key'         => 'username',
+				'description' => 'Column is not using same collation as table'
+			],
+		];
+
+		$this->assertSame($expected, $result);
+	}
 }
